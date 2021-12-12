445f45bfb201af62fa6bd324cdef11aac73ff4ad
Tirth Gajjar <contact@tirth.dev>
2021-12-12 16:10:06 +0530

    updated styles

--- app/components/interaction-card/index.tsx
+++ app/components/interaction-card/index.tsx
@@ -7,9 +7,8 @@ const articles = [
     id: 1,
     title: "Bottom navigation drag utility",
     href: "/1",
-    price: "$48",
     source: {
-      logo: "//logo.clearbit.com/knowuser.com",
+      logo: "https://basecamp.com/basecamp-icon-196x196.png",
       name: "Basecamp",
     },
     author: {
@@ -22,16 +21,11 @@ const articles = [
     },
     views: "1.2k",
     backgroundColorClass: "bg-yellow-50",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-    imageAlt:
-      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
   },
   {
     id: 2,
-    title: "Nomad Tumbler",
+    title: "Collapse side navigation on-hover",
     href: "/2",
-    price: "$35",
     source: {
       logo: "//logo.clearbit.com/loom.com",
       name: "Loom",
@@ -46,16 +40,11 @@ const articles = [
     },
     views: "1.2k",
     backgroundColorClass: "bg-indigo-50",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-02.jpg",
-    imageAlt:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
   },
   {
     id: 3,
-    title: "Focus Paper Refill",
+    title: "Icon transition micro animation",
     href: "/3",
-    price: "$89",
     source: {
       logo: "//logo.clearbit.com/telegram.org",
       name: "Telegram",
@@ -70,16 +59,11 @@ const articles = [
     },
     views: "1.2k",
     backgroundColorClass: "bg-gray-200",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-    imageAlt:
-      "Person using a pen to cross a task off a productivity paper card.",
   },
   {
     id: 4,
-    title: "Machined Mechanical Pencil",
+    title: "Swipe down to refresh animation",
     href: "4",
-    price: "$35",
     source: {
       logo: "//logo.clearbit.com/dribbble.com/",
       name: "Dribbble",
@@ -94,10 +78,6 @@ const articles = [
     },
     views: "1.2k",
     backgroundColorClass: "bg-orange-50",
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
   },
   // More products...
 ]
@@ -106,22 +86,22 @@ export default function InteractionCard() {
   return (
     <div className="bg-white">
       <div className="px-4 py-8 sm:px-6 sm:py-8 lg:px-0">
-        <h2 className="sr-only">Products</h2>
-
+        <h2 className="sr-only">artciles</h2>
         <div className="grid gap-x-6 gap-y-10 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-8 xl:grid-cols-4">
           {articles.map(article => (
-            <a key={article.id} href={article.href}>
+            <div key={article.id}>
               <div className="w-full bg-gray-200 rounded-lg overflow-hidden relative">
                 {/* <img
                   src={article.imageSrc}
                   alt={article.imageAlt}
                   className="w-full h-full hover:opacity-75 object-cover object-center"
                 /> */}
-                <div
+                <a
                   className={classNames(
                     article.backgroundColorClass,
-                    "aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 h-60 w-full hover:opacity-75",
+                    "aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 h-60 w-full hover:opacity-75 block",
                   )}
+                  href={article.href}
                 >
                   <div className="p-3">
                     <a
@@ -136,10 +116,10 @@ export default function InteractionCard() {
                       <span>{article.source.name}</span>
                     </a>
                   </div>
-                </div>
+                </a>
               </div>
               <h3 className="mt-3 text-gray-700 text-sm font-semibold">
-                {article.title}
+                <a href={article.href}>{article.title}</a>
               </h3>
               <div className="flex text-xs mt-1 space-x-1 text-gray-400">
                 <a href={article.author.profile} className="text-indigo-700">
@@ -160,7 +140,7 @@ export default function InteractionCard() {
                   <span>{article.comments.count}</span>
                 </button>
               </div>
-            </a>
+            </div>
           ))}
         </div>
       </div>
