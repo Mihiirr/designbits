627ac2e4f9c9748515c872f63ac21f54385b10a2
Tirth Gajjar <contact@tirth.dev>
2022-02-01 11:39:29 +0530

    ok

--- apps/designbits/rollup.config.js
+++ apps/designbits/rollup.config.js
@@ -2,7 +2,6 @@ import commonJs from "@rollup/plugin-commonjs"
 import jsonPlugin from "@rollup/plugin-json"
 import resolve from "@rollup/plugin-node-resolve"
 import * as path from "path"
-import copy from "rollup-plugin-copy"
 
 export default {
   input: "deploy/index.js",
@@ -12,14 +11,6 @@ export default {
   },
   external: ["fs/promises"],
   plugins: [
-    copy({
-      targets: [
-        {
-          src: "../../node_modules/.prisma/client/schema.prisma",
-          dest: "api",
-        },
-      ],
-    }),
     commonJs(),
     resolve({
       moduleDirectories: ["node_modules"],
