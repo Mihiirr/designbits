242ac59e64a9af671c6f1ffb15d48e6bf23910b7
Tirth Gajjar <contact@tirth.dev>
2022-02-03 09:43:58 +0530

    try with esm version

--- apps/designbits/rollup.config.js
+++ apps/designbits/rollup.config.js
@@ -7,7 +7,7 @@ export default {
   input: "deploy/index.js",
   output: {
     file: "api/index.js",
-    format: "cjs",
+    format: "esm",
   },
   inlineDynamicImports: true,
   external: ["fs/promises", "_http_common", "encoding"],
