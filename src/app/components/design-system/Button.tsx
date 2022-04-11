1726f1c7324fb63936c79c7f074ffbfcdde06550
Mihir Patel <patelmihir5398@gmail.com>
2022-04-11 17:42:59 +0530

    solved some responsive issues

--- src/app/components/design-system/Button.tsx
+++ src/app/components/design-system/Button.tsx
@@ -1,27 +1,128 @@
+import clsx from "clsx"
 import React, { HTMLAttributes } from "react"
+import { Link } from "remix"
 
-type ButtonSizes = "LARGE" | "MEDIUM" | "SMALL"
+type ButtonSizes = "large" | "medium" | "small"
+type Rounded = "regular" | "full"
 
-interface Props extends HTMLAttributes<HTMLButtonElement> {
+type CommonProps<T extends HTMLElement> = {
   icon?: React.ComponentType<any>
-  size: ButtonSizes
+  size?: ButtonSizes
+  rounded?: Rounded
+} & HTMLAttributes<T>
+
+type ButtonElementProps = CommonProps<HTMLButtonElement> & {
+  type?: "primary" | "outline" | "ghost"
 }
 
-const LargeVariantStyles = {
-  spacing: "px-5 py-2",
-  text: "text-base font-normal leading-normal",
-  background: "bg-indigo-700 text-white",
-  border: "rounded border border-transparent",
-  outline: "outline outline-offset-0",
-  hover: "hover:bg-indigo-900",
-  focus: "focus:border-indigo-900 focus:outline-indigo-300",
-  active: "active:border-indigo-900 active:bg-indigo-900 active:outline-2",
-  disabled: "disabled:bg-opacity-50 disabled:opacity-50",
+type AnchorElementProps = CommonProps<HTMLAnchorElement> & {
+  type: "link"
+  href: string
 }
 
-const Button: React.FC<Props> = ({ children, ...rest }: Props) => {
+type Props = ButtonElementProps | AnchorElementProps
+
+const TypeVariants: {
+  [type in NonNullable<Props["type"]>]: Record<string, string>
+} = {
+  primary: {
+    background: "bg-indigo-700 text-white",
+    outline: "outline outline-[3px] outline-offset-0",
+    hover: "hover:bg-indigo-900",
+    active: "active:border-indigo-900 active:bg-indigo-900 active:outline-2",
+    focus: "focus:border-indigo-900 focus:outline-indigo-300",
+  },
+  outline: {
+    background: "bg-white text-indigo-700",
+    outline: "outline outline-1 outline-offset-0",
+    hover: "hover:bg-indigo-900 hover:text-white",
+    active: "active:border-indigo-900 active:bg-indigo-900 active:outline-2",
+    focus: "focus:border-indigo-900 focus:outline-indigo-300",
+  },
+  ghost: {
+    background: "bg-white text-indigo-700",
+    hover: "hover:bg-indigo-50",
+    active:
+      "active:outline outline-offset-0 active:bg-indigo-50 active:outline-2",
+    focus: "focus:outline-indigo-300 focus:bg-indigo-50",
+  },
+  link: {
+    background: "text-gray-900",
+    hover: "hover:text-indigo-700",
+    active:
+      "active:text-indigo-700 active:outline outline-offset-0 active:outline-2",
+    focus: "focus:outline-indigo-300",
+  },
+}
+
+const SizeVariantStyles: {
+  [size in ButtonSizes]: Record<string, string>
+} = {
+  large: {
+    spacing: "px-5 py-2",
+    text: "text-base font-medium leading-normal",
+    disabled: "disabled:bg-opacity-50 disabled:opacity-50",
+  },
+  medium: {
+    spacing: "px-5 py-2",
+    text: "text-base font-normal leading-normal",
+    disabled: "disabled:bg-opacity-50 disabled:opacity-50",
+  },
+  small: {
+    spacing: "px-5 py-2",
+    text: "text-base font-normal leading-normal",
+    disabled: "disabled:bg-opacity-50 disabled:opacity-50",
+  },
+}
+
+const RoundedVariantStyles: {
+  [rounded in Rounded]: Record<string, string>
+} = {
+  regular: {
+    border: "rounded border border-transparent",
+  },
+  full: {
+    border: "rounded-full border border-transparent",
+  },
+}
+
+const isLink = (props: Props): props is AnchorElementProps => {
+  return props.type === "link"
+}
+
+const Button: React.FC<Props> = (props: Props) => {
+  const {
+    children,
+    size = "medium",
+    rounded = "regular",
+    type = "primary",
+  } = props
+  const sizeStyles = SizeVariantStyles[size]
+  const borderStyles = RoundedVariantStyles[rounded]
+  const typeStyles = TypeVariants[type]
+
+  if (isLink(props)) {
+    return (
+      <a
+        className={clsx(
+          Object.values({ ...sizeStyles, ...borderStyles, ...typeStyles }),
+        )}
+        {...props}
+      >
+        {children}
+      </a>
+    )
+  }
+
+  const { type: htmlButtonType, ...rest } = props
+
   return (
-    <button className={Object.values(LargeVariantStyles).join(" ")} {...rest}>
+    <button
+      className={clsx(
+        Object.values({ ...sizeStyles, ...borderStyles, ...typeStyles }),
+      )}
+      {...rest}
+    >
       {children}
     </button>
   )
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,11 +1,19 @@
 import { Disclosure } from "@headlessui/react"
 import { MenuIcon, XIcon } from "@heroicons/react/outline"
 import React from "react"
-import { Link } from "remix"
+import { Link, LoaderFunction, useLoaderData } from "remix"
 import Button from "~/components/design-system/Button"
 import DesignBitsLogo from "~/components/Logo"
+import { getLoginInfoSession } from "~/services/auth/login.server"
+import { getLoggedInUser } from "~/services/auth/session.server"
+import { formatInteractionPostsData } from "~/services/db/formatters.server"
+import { findInteractionsForCategory } from "~/services/db/queries/post.server"
+import { OkResponse } from "~/utils/response-helpers.server"
+import { ResponseData } from "./explore/$category"
 
 type Props = {}
+
+type LoaderData = ResponseData<ReturnType<typeof formatInteractionPostsData>>
 
 const navigation = [
   { name: "Pricing", href: "/pricing" },
@@ -15,32 +23,50 @@ const navigation = [
   },
 ]
 
+export const loader: LoaderFunction = async ({ params, request }) => {
+  const session = await getLoginInfoSession(request)
+  const user = await getLoggedInUser(request)
+
+  const interactions = formatInteractionPostsData(
+    await findInteractionsForCategory({
+      userId: user?.id,
+    }),
+  )
+
+  return OkResponse({
+    data: { interactions },
+    errors: [],
+    headers: await session.getHeaders(),
+  })
+}
+
 const Landing = (props: Props) => {
+  const { data } = useLoaderData<LoaderData>()
+  console.log({ data })
   return (
     <div>
       {/* Header */}
       <Disclosure as="nav">
         {({ open }) => (
           <>
-            <div className="mx-auto flex max-w-7xl items-center justify-between p-4 sm:px-0">
+            <div className=" mx-auto flex max-w-sm items-center justify-between p-4 md:max-w-2xl md:p-4 lg:max-w-4xl lg:p-4 xl:max-w-7xl xl:py-4 xl:px-0 2xl:max-w-screen-2xl">
               <DesignBitsLogo />
               <div className="flex items-center">
-                <div className="hidden items-center sm:-my-px sm:flex">
-                  {navigation.map(item => (
-                    <Link
-                      key={item.name}
-                      to={item.href}
-                      className="mr-8 h-5 font-medium text-gray-800"
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
-                  <Link
-                    to="/auth/login"
-                    className="flex h-10 w-24 items-center justify-center rounded-lg border bg-white font-semibold text-indigo-700"
-                  >
-                    SIGN IN
-                  </Link>
+                <div className="hidden w-[300px] items-center justify-between sm:-my-px sm:flex">
+                  <div>
+                    {navigation.map(item => (
+                      <Button
+                        size="large"
+                        rounded="regular"
+                        type="link"
+                        href={item.href}
+                        key={item.name}
+                      >
+                        {item.name}
+                      </Button>
+                    ))}
+                  </div>
+                  <Button size="large">SIGN IN </Button>
                 </div>
                 <div className="-mr-2 flex items-center sm:hidden">
                   {/* Mobile menu button */}
@@ -70,8 +96,8 @@ const Landing = (props: Props) => {
       </Disclosure>
 
       {/* body */}
-      <div className="mx-auto mt-20 max-w-sm text-left text-indigo-700 sm:max-w-7xl">
-        <p className="text-4xl font-light leading-tight sm:text-6xl">
+      <div className="mx-auto mt-20 max-w-sm text-left text-indigo-700 sm:max-w-xs md:max-w-2xl lg:max-w-4xl xl:max-w-7xl 2xl:max-w-screen-2xl">
+        <p className="mb-14 text-4xl font-light leading-tight sm:text-6xl">
           Your companion for
           <br />
           <span className="text-4xl font-bold sm:text-6xl">
@@ -81,15 +107,12 @@ const Landing = (props: Props) => {
           from the industry
         </p>
         {/* Try it for free button */}
-        <Link
-          to="/auth/login"
-          className="mt-14 flex h-12 w-36 items-center justify-center rounded-full bg-indigo-700 text-white"
-        >
+        <Button size="large" rounded="full">
           Try it for free
-        </Link>
+        </Button>
       </div>
 
-      <div className="mx-auto max-w-sm sm:max-w-7xl">
+      <div className="mx-auto w-full md:max-w-2xl lg:max-w-4xl xl:max-w-7xl 2xl:max-w-screen-2xl">
         {/* Explore by UI patterns */}
         <ExploreLanding
           title="Explore by UI pattern"
@@ -121,12 +144,12 @@ const Landing = (props: Props) => {
       </div>
 
       {/* What else */}
-      <div className="mx-auto mt-24 h-auto max-w-sm text-left sm:max-w-7xl ">
+      <div className="mx-auto mt-24 h-auto max-w-sm text-left md:max-w-2xl lg:max-w-4xl xl:max-w-7xl 2xl:max-w-screen-2xl">
         <p className="text-4xl font-bold text-indigo-700 sm:text-7xl">
           What else?
         </p>
-        <div className="mt-20 h-auto w-full sm:flex sm:flex-wrap">
-          <div className="mb-24 h-auto w-full sm:mb-36 sm:flex sm:w-1/2">
+        <div className="mt-20 h-auto w-full xl:flex xl:flex-wrap">
+          <div className="mb-24 h-auto w-full md:flex md:w-full xl:w-2/4">
             <div className="mb-5 h-28 w-28 rounded-full border bg-purple-300 sm:mb-0"></div>
             <div className="w-96 sm:ml-5 sm:px-4">
               <p className="text-2xl font-semibold text-[#3F3665]">
@@ -139,28 +162,26 @@ const Landing = (props: Props) => {
               </p>
             </div>
           </div>
-
-          <div className="mb-24 w-full sm:mb-0 sm:flex sm:w-1/2 sm:pl-12">
+          <div className="mb-24 h-auto w-full md:flex md:w-full xl:w-2/4">
             <div className="mb-5 h-28 w-28 rounded-full border bg-purple-300 sm:mb-0"></div>
             <div className="w-96 sm:ml-5 sm:px-4">
               <p className="text-2xl font-semibold text-[#3F3665]">
                 Inspirations from geeks across the world
               </p>
-              <p className="mt-5 text-base font-normal text-[#593C71] sm:mt-6">
+              <p className="text-base font-normal text-[#593C71] sm:mt-6">
                 Interactions categorised by UI pattern and flows. Added with
                 filter based on industry like Fintech, Social media, Delievery
                 plaftorms, Heathcare, etc.
               </p>
             </div>
           </div>
-
-          <div className="w-full sm:flex sm:w-1/2">
+          <div className="mb-24 h-auto w-full md:flex md:w-full xl:w-2/4">
             <div className="mb-5 h-28 w-28 rounded-full border bg-purple-300 sm:mb-0"></div>
             <div className="w-96 sm:ml-5 sm:px-4">
               <p className="text-2xl font-semibold text-[#3F3665]">
                 Exchange views & insights
               </p>
-              <p className="mt-5 text-base font-normal text-[#593C71] sm:mt-6">
+              <p className="text-base font-normal text-[#593C71] sm:mt-6">
                 Use comments and surveys for healthy discussions that improve
                 our collective knowledge and perspectives
               </p>
@@ -192,11 +213,11 @@ const Landing = (props: Props) => {
             Signup
           </Link>
         </div>
-        <div className="mt-28 h-auto w-4/5 justify-between sm:flex">
+        <div className="mt-28 h-auto w-4/5 justify-between sm:flex 2xl:max-w-screen-2xl">
           <DesignBitsLogo />
           <div>logos</div>
         </div>
-        <div className="mt-10 h-auto w-4/5 justify-between sm:flex">
+        <div className="mt-10 h-auto w-4/5 justify-between lg:flex 2xl:max-w-screen-2xl">
           <div className="mb-8 flex flex-col text-sm font-normal text-[#928EE1] sm:flex sm:flex-row">
             <Link to="#" className="mr-5">
               About us
@@ -230,27 +251,34 @@ type ExploreLandingProps = {
 
 export const ExploreLanding = (props: ExploreLandingProps) => {
   return (
-    <div className=" mt-24 flex-col text-gray-800">
-      <p className="mb-2 text-2xl font-bold sm:text-5xl">{props.title}</p>
-      <p className="max-w-3xl text-base font-normal">{props.description}</p>
-      <div className="mt-4 flex h-8 w-full items-center overflow-x-scroll text-indigo-700 sm:overflow-hidden">
-        <Link
-          to="#"
-          className="mr-8 inline-block h-8 w-auto items-center justify-center whitespace-nowrap rounded-full bg-indigo-100 px-4"
-        >
-          {props.btn1}
-        </Link>
-        <Link to="#" className="mr-8 inline-block whitespace-nowrap">
-          {props.btn2}
-        </Link>
-        <Link to="#" className="mr-8 inline-block whitespace-nowrap">
-          {props.btn3}
-        </Link>
-        <Link to="/explore/all" className="inline-block whitespace-nowrap">
-          {props.seeMore}
-        </Link>
+    <div className="mt-24 flex-col text-gray-800">
+      <div className="mx-auto max-w-sm md:max-w-full">
+        <p className="mb-2 text-2xl font-bold sm:text-5xl">{props.title}</p>
+        <p className="max-w-3xl text-base font-normal">{props.description}</p>
       </div>
-      <div className="mt-5 flex flex-nowrap overflow-scroll sm:h-auto sm:w-full sm:justify-between sm:overflow-hidden">
+      <div className="mt-4 flex items-center overflow-x-scroll py-3 px-1 pl-4">
+        <div className="mr-1 inline-block whitespace-nowrap rounded-full">
+          <Button size="medium" type="ghost" rounded="full">
+            {props.btn1}
+          </Button>
+        </div>
+        <div className="mr-1 inline-block whitespace-nowrap">
+          <Button size="medium" type="ghost" rounded="full">
+            {props.btn2}
+          </Button>
+        </div>
+        <div className="mr-1 inline-block whitespace-nowrap">
+          <Button size="medium" type="ghost" rounded="full">
+            {props.btn3}
+          </Button>
+        </div>
+        <div className="mr-1 inline-block whitespace-nowrap">
+          <Button size="medium" type="link" href="/explore/all" rounded="full">
+            {props.seeMore}
+          </Button>
+        </div>
+      </div>
+      <div className="mt-5 flex flex-nowrap overflow-scroll pl-4 sm:h-auto sm:w-full sm:justify-between">
         <div className="mr-5 rounded-lg bg-green-300 p-36">AAAAA</div>
         <div className="mr-5 rounded-lg bg-green-300 p-36">BBBBB</div>
         <div className="mr-5 rounded-lg bg-green-300 p-36">CCCCC</div>
